646c7bb72722093fc0a8c0cda30b6ec601f1d6eb
Luca Milanesio <luca.milanesio@gmail.com>
2024-11-04 21:04:05 +0000

    Update gerrit@3.11.0 to rc2
    Change-Id: I1ce47baf90aa9a18545319a1094c0e5368f3f252

--- gerrit@3.11.0.rb
+++ gerrit@3.11.0.rb
@@ -4,9 +4,9 @@
 class GerritAT3110 < Formula
   desc ""
   homepage "https://www.gerritcodereview.com"
-  version "3.11.0-rc1"
-  url "https://tarball.gerritforge.com/gerrit-3.11.0-rc1.tar.gz"
-  sha256 "5f8d713439b2ceab8cb085671018de1237beb3e2aaa15a4827b61693703c018d"
+  version "3.11.0-rc2"
+  url "https://tarball.gerritforge.com/gerrit-3.11.0-rc2.tar.gz"
+  sha256 "e74ca53699609d15a7af14d6f5f326b0eb06dbc8344b3a8c2424cca2c8301ff1"
   
   depends_on "openjdk@21"
   depends_on "git"
