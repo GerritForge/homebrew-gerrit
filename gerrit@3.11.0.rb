982f77dd2ab60aaca6c3c00b270b19ec55511b83
Daniele Sassoli <danielesassoli@gmail.com>
2024-11-29 16:24:05 +0000

    Modularize creation of formula
    Base step to extract generic variables that we can then reuse for
    different version of gerrit in different formulas
    
    Change-Id: Idb9a799c067bd53016e35ed3a9b55f733b551c37

--- gerrit@3.11.0.rb
+++ gerrit@3.11.0.rb
@@ -1,40 +1,10 @@
 # typed: false
 # frozen_string_literal: true
 
-class GerritAT3110 < Formula
-  desc ""
-  homepage "https://www.gerritcodereview.com"
+class GerritAT3110 < GerritBase
+
+  java_version = "21"
   version "3.11.0-rc5"
-  url "https://tarball.gerritforge.com/gerrit-3.11.0-rc5.tar.gz"
   sha256 "97bdaf49d49cd355a9e671b5e5935823ed94950f136081d16c83579418c266d7"
-
-  depends_on "openjdk@21"
-  depends_on "git"
-  depends_on "openssh"
-
-  def install
-    mkdir_p prefix/"plugins"
-    (prefix/"plugins").install "plugins/uploadvalidator.jar"
-    (prefix/"plugins").install "plugins/avatars-gravatar.jar"
-
-    bin.install "bin/gerrit.war"
-    lib.install "lib/out-of-the-box.jar"
-
-    mkdir_p var/"gerrit"
-    mkdir_p var/"gerrit"/"etc"
-    (var/"gerrit"/"etc").install "etc/gerrit.config"
-
-    (var/"gerrit/plugins").install_symlink Dir[prefix/"plugins/*"]
-    (var/"gerrit/lib").install_symlink Dir[lib/"*"]
-
-    system "java", "-jar", bin/"gerrit.war", "init", "-d", (var/"gerrit"), "--batch", "--install-all-plugins", "--no-auto-start"
-    ohai ""
-    ohai "Gerrit site initialized in " + (var/"gerrit")
-    ohai "---------------------------------------------------------"
-    ohai " * To start Gerrit: " + (var/"gerrit/bin/gerrit.sh start")
-    ohai " * Gerrit will then be available at http://localhost:8080"
-    ohai ""
-    ohai " * To stop  Gerrit: " + (var/"gerrit/bin/gerrit.sh stop")
-    ohai ""
-  end
+  url generate_url(version)
 end
