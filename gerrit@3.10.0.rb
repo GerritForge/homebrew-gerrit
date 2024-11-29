7963f80269043090620796b16a8984bdab3bcbda
Daniele Sassoli <danielesassoli@gmail.com>
2024-11-29 16:20:14 +0000

    Modularize creation of formula
    Base step to extract generic variables that we can then reuse for
    different version of gerrit in different formulas
    
    Change-Id: Idb9a799c067bd53016e35ed3a9b55f733b551c37

--- gerrit@3.10.0.rb
+++ gerrit@3.10.0.rb
@@ -1,10 +1,13 @@
 # typed: false
 # frozen_string_literal: true
 
-class GerritAT3110 < GerritBase
+require_relative 'gerrit_base'
 
-  java_version = "21"
-  version "3.11.0-rc5"
+class GerritAT3100 < GerritBase
+
+  java_version = "17"
+  version "3.10.0"
   sha256 "97bdaf49d49cd355a9e671b5e5935823ed94950f136081d16c83579418c266d7"
   url generate_url(version)
+  
 end
